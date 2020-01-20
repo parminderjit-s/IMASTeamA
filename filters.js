47d96dcf85b1528fefd1ceafea6b9232a17447b4
Parminder <parminderjit.s@yahoo.com>
2020-01-20 10:58:02 +0800

    Signed-off-by: Parminder <parminderjit.s@yahoo.com>

---
+++ filters.js
@@ -0,0 +1,11 @@
+$('[data-search]').on('keyup', function() {
+	var searchVal = $(this).val();
+	var filterItems = $('[data-filter-item]');
+
+	if ( searchVal != '' ) {
+		filterItems.addClass('hidden');
+		$('[data-filter-item][data-filter-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+	} else {
+		filterItems.removeClass('hidden');
+	}
+});
